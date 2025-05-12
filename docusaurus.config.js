5ac6739c096e05d9ac73aaad1d0778c35883be33
Gerardo Roa <gerardo.roa@gmail.com>
2025-05-12 13:36:54 +0100

    announcement bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,7 +90,7 @@ module.exports = {
                 "📆 <a href='https://cadenzachallenge.org/cadenza2025-workshop/' target='_blank'>Cadenza Workshop 2025</a>  - <a href='https://ica2025neworleans.org/' target='_blank'>ICA 2025 Special Session</a> - 22th May, 15:15-17:00 (GMT-5), New Orleans, Louisiana, USA‼️",
             backgroundColor: '#a2dea2',
             textColor: '#070707',
-            isCloseable: true,
+            isCloseable: false,
         },
         blog: {
             sidebar: {
