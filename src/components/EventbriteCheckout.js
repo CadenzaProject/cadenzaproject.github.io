387857ad94d25f4eb1aec3400bde1d26068069cc
Gerardo Roa <gerardo.roa@gmail.com>
2026-06-18 10:57:03 +0100

    ticket page

---
+++ src/components/EventbriteCheckout.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+
+export default function EventbriteCheckout() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://www.eventbrite.co.uk/static/widgets/eb_widgets.js';
+        script.async = true;
+
+        script.onload = () => {
+            if (!window.EBWidgets) return;
+
+            window.EBWidgets.createWidget({
+                widgetType: 'checkout',
+                eventId: '1991542799123',
+                iframeContainerId: 'eventbrite-widget-container-1991542799123',
+                iframeContainerHeight: 425,
+                onOrderComplete: () => {
+                    console.log('Order complete!');
+                },
+            });
+        };
+
+        document.body.appendChild(script);
+    }, []);
+
+    return (
+        <div id="eventbrite-widget-container-1991542799123" />
+    );
+}
